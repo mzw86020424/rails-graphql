0d0de42057e86b65ed8a2098d9fae66bbd6ae649
mzw86020424 <mzw86020424@gmail.com>
2022-11-13 13:46:44 +0900

    usersのアクションを一通り実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:index, :create] do
+    member do
+      get :show
+    end
+  end
 end
